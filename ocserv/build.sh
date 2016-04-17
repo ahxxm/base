8a377f446e86714d00d731b36795803d2246fd81
ahxxm <i@ahxxm.com>
2016-04-17 22:22:56 +0800

    add missing protobuf-c

--- ocserv/build.sh
+++ ocserv/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # dep
 dep="libev-dev gnutls-dev linux-pam-dev lz4-dev"
-build_dep="build-base curl autoconf automake readline-dev libnl3-dev linux-headers gperf"
+build_dep="build-base curl autoconf automake readline-dev libnl3-dev linux-headers gperf protobuf-c-dev"
 
 apk update
 apk add $build_dep $dep
@@ -13,7 +13,7 @@ apk add $build_dep $dep
 cd
 curl -fSL https://github.com/ahxxm/base/raw/master/ocserv/ocserv-0.10.11.zip -o ocserv.zip
 unzip -q ocserv.zip
-mv ocserv-* ocserv 
+mv ocserv-* ocserv
 cd ocserv
 
 sed -i '/#define DEFAULT_CONFIG_ENTRIES /{s/96/200/}' src/vpn.h
@@ -27,4 +27,3 @@ make install
 apk del $build_dep
 rm /var/cache/apk/*
 rm -r /root/*
-

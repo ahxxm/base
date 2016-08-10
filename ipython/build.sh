3bb2ce97cc120ede5459f56a8fae13fe0ae01f20
ahxxm <i@ahxxm.com>
2016-08-10 12:38:48 +0800

    add pandas and matplotlib

--- ipython/build.sh
+++ ipython/build.sh
@@ -3,3 +3,5 @@ apk add --update python py-pip openssl python3 python-dev
 apk add --update build-base zeromq-dev
 ln -s /usr/include/locale.h /usr/include/xlocale.h
 pip install --no-cache-dir "ipython[all]"
+pip install --no-cache-dir pandas matplotlib
+apk del build-base zeromq-dev

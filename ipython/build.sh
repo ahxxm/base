22a8452e8e3bd36399bac43f8cd8c1253a03b18e
ahxxm <i@ahxxm.com>
2016-08-10 12:15:43 +0800

    add build.sh, disable pip cache

--- ipython/build.sh
+++ ipython/build.sh
@@ -2,4 +2,4 @@
 apk add --update python py-pip openssl python3 python-dev
 apk add --update build-base zeromq-dev
 ln -s /usr/include/locale.h /usr/include/xlocale.h
-pip install "ipython[all]"
+pip install --no-cache-dir "ipython[all]"

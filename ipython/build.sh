8b01f5618157e88c6eda1d60790280b83f53f64e
ahxxm <i@ahxxm.com>
2016-08-10 12:46:47 +0800

    try register ipython3 kernel

--- ipython/build.sh
+++ ipython/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-apk add --update python py-pip openssl python3 python-dev
-apk add --update build-base zeromq-dev
+apk add --update python py-pip openssl python3 
+apk add --update build-base zeromq-dev python-dev python3-dev
 ln -s /usr/include/locale.h /usr/include/xlocale.h
 pip install --no-cache-dir "ipython[all]"
 pip install --no-cache-dir pandas matplotlib
-apk del build-base zeromq-dev
+
+# python3 register?
+python3 -m venv 3
+source 3/bin/activate
+pip install --no-cache-dir ipykernel pandas matplotlib
+ipython3 kernel install
+
+apk del build-base zeromq-dev python-dev python3-dev

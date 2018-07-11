ebcbfd6e555b9a8512b754dc477f37ba30c10a46
ahxxm <i@ahxxm.com>
2018-07-11 20:57:29 +0800

    bump nginx

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -4,7 +4,7 @@ set -e
 
 # ref:
 # - https://www.futures.moe/writings/configure-nginx-with-security-and-effective-yes-or-no.htm
-NGINX_VERSION=1.15.0
+NGINX_VERSION=1.15.1
 LIBRESSL_VERSION=2.7.4
 
 mkdir -p /tmp

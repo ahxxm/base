4969ed91f044d9a49cff7a1f0120c1209d3d4b4c
ahxxm <i@ahxxm.com>
2020-06-21 17:55:34 +0800

    bump v2ray to 4.25.0, fix bazel build

--- v2ray/build.sh
+++ v2ray/build.sh
@@ -15,10 +15,13 @@ chmod +x bazel-installer.sh
 # fix HEAD
 go get -insecure -v -t -d v2ray.com/core/...
 cd ./src/v2ray.com/core
-git checkout tags/v4.23.2
+git checkout tags/v4.25.0
+
+# update geoip geosite dat
+./release/updatedat.sh
 
 # build for linux-amd64
-$HOME/bin/bazel build --action_env=GOPATH=$GOPATH --action_env=PATH=$PATH //release:v2ray_linux_amd64_package
+$HOME/bin/bazel build --action_env=GOPATH=$GOPATH --action_env=PATH=$PATH --action_env=GPG_PASS=${SIGN_KEY_PASS} --action_env=SPWD=$PWD --action_env=GOCACHE=$(go env GOCACHE) --spawn_strategy local //release:v2ray_linux_amd64_package
 
 # unzip to /v2ray for runner to copy
 mkdir -p /v2ray && cd /v2ray

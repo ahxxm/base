2e50e9b088c4940121c4dcba33cbd1c0456afd99
ahxxm <i@ahxxm.com>
2021-05-14 10:31:52 +0800

    ss Support non-standard AEAD ciphers

--- shadowsocks/build.sh
+++ shadowsocks/build.sh
@@ -9,7 +9,7 @@ apk add git musl-dev openssl-dev file make
 git clone https://github.com/shadowsocks/shadowsocks-rust
 cd shadowsocks-rust
 git checkout v1.10.9
-cargo build --target=x86_64-unknown-linux-musl --features "local-redir trust-dns" --release
+cargo build --target=x86_64-unknown-linux-musl --features "local-redir trust-dns aead-cipher-extra" --release
 
 # move out for copy stage
 cp target/x86_64-unknown-linux-musl/release/ss* /workdir

98264d8039af2023e9ef0e30182c71b52937beb5
ahxxm <i@ahxxm.com>
2026-02-22 16:58:13 +0900

    bump

--- shadowsocks/build.sh
+++ shadowsocks/build.sh
@@ -8,8 +8,8 @@ cd /workdir
 apk add git musl-dev openssl-dev file make
 git clone https://github.com/shadowsocks/shadowsocks-rust
 cd shadowsocks-rust
-git checkout v1.20.1
-cargo build --target=x86_64-unknown-linux-musl --features "local-redir trust-dns local-dns aead-cipher-extra" --release
+git checkout v1.24.0
+cargo build --target=x86_64-unknown-linux-musl --features "full" --release
 
 # move out for copy stage
 cp target/x86_64-unknown-linux-musl/release/ss* /workdir
